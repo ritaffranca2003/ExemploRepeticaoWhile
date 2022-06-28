9a0b233d242cd4521ceca965ab391a45395ba735
foxca <foxca@LAPTOP-UTQBBQF9>
2022-06-28 16:58:50 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploRepeticao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, cont = 0;
+            double n1, n2;
+            Console.WriteLine("Digite a quantidade de alunos para calcular a média");
+            num = int.Parse(Console.ReadLine());
+            while (cont < num)
+            {
+                Console.WriteLine("Digite a primeira nota");
+                n1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a segunda nota");
+                n2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Média=" + ((n1 + n2) /2) + "\n");
+                cont++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ExemploRepeticao/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ExemploRepeticao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExemploRepeticao")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("b05a52b9-c05d-44db-aa57-d42198c5f44b")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
